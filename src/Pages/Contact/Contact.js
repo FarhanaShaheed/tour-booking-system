665dc91b4a94c73dbcfa5f61f633ef8bb5b72154
FarhanaShaheed <farhana15-734@diu.edu.bd>
2021-11-21 17:29:00 +0600

    google map added

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faBuilding,faCheck,faPhone,faInbox, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import googleMap from '../../Images/google-map.jfif';
 import './Contact.css';
+import GoogleMap from './GoogleMap';
 
 const Contact = () => {
     return (
         <div>
-            <Container className="contact-class">
-        
+            <Container className="contact-class mt-5">
+                <h2>Contact Us for Any Query</h2>
               <Row className="m-5">
-              <Col xs={12} md={6}>
+              <Col xs={12} md={6} className="message-class">
                <h2>Drop us a line</h2>
                <form action="">
                    <input type="text" placeholder="Name"/>
@@ -23,24 +23,25 @@ const Contact = () => {
                </form>
               </Col>
               <Col xs={12} md={6}>
-                  <img style={{width:'100%'}} src={googleMap} alt="" fluid/>
+                  <GoogleMap></GoogleMap>
               </Col>
               </Row>
               <div className="row d-flex align-items-center justify-content-center">
                   
                   <div className="col-sm-6">
-                     <h3><FontAwesomeIcon icon={faMapMarkerAlt}/> Address: <span>Avenue 234</span></h3>
-                     <h3><FontAwesomeIcon icon={faBuilding} /> City: <span>New York-US</span></h3>
-                     <h3><FontAwesomeIcon icon={faCheck} /> Check-In: <span>15:00 am</span></h3>
+                     <h4><FontAwesomeIcon icon={faMapMarkerAlt}/> Address: <span>Avenue 234</span></h4>
+                     <h4><FontAwesomeIcon icon={faBuilding} /> City: <span>New York-US</span></h4>
+                     <h4><FontAwesomeIcon icon={faCheck} /> Check-In: <span>15:00 am</span></h4>
                      
                   </div>
                   <div  className="col-sm-6">
-                  <h3><FontAwesomeIcon icon={faPhone} /> Phone: <span>374765290293</span></h3>
-                     <h3><FontAwesomeIcon icon={faInbox} /> Email: <span>info@travel.com</span></h3>
-                     <h3><FontAwesomeIcon icon={faTimes} /> Check-Out: <span>11:00am</span></h3>
+                  <h4><FontAwesomeIcon icon={faPhone} /> Phone: <span>374765290293</span></h4>
+                     <h4><FontAwesomeIcon icon={faInbox} /> Email: <span>info@travel.com</span></h4>
+                     <h4><FontAwesomeIcon icon={faTimes} /> Check-Out: <span>11:00am</span></h4>
                   </div>
                   
               </div>
+             
             </Container>
         </div>
     );
